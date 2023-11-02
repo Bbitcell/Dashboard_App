bf45d68f945d714925507185bddf6e2a0fea7f0f
Bbitcell <74414621+Bbitcell@users.noreply.github.com>
2023-11-02 13:58:56 +0300

    Update Dashboard_New.py

--- Dashboard_New.py
+++ Dashboard_New.py
@@ -141,7 +141,9 @@ project_creation = st.button("Add New Project", type="primary")
 if project_creation:
     project_created = True
     df2 = pd.DataFrame(columns=st.session_state["main"].columns)
-    df2 = pd.concat([df2, {'Projects': 'New Project'}])
+
+    df3 = pd.DataFrame({'Projects': 'New Project'})
+    df2 = pd.concat([df2, df3])
     st.session_state["main"] = df2
 
 bk2 = st.session_state["main"].copy()
@@ -285,7 +287,8 @@ st.session_state["main"] = bk2.copy()
 if pd.notna(st.session_state["main"].iloc[-1][st.session_state["main"].columns[5:]]).any().any():
     project_name = st.session_state["main"]["Projects"][prj_index]
 
-    st.session_state["main"] = pd.concat([st.session_state["main"], {'Projects': project_name}])
+    prj_df_new = pd.DataFrame({'Projects': project_name})
+    st.session_state["main"] = pd.concat([st.session_state["main"], prj_df_new])
 if not st.session_state["main"].equals(bk):
     st.rerun()
 
